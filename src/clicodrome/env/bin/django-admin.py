e24a227c6835267b2d1ec2d6e383e6450feafcbe
droussel <damien.roussel.1@etu.u-bordeaux.fr>
2020-02-18 21:23:28 +0100

    renettoyage depot git

--- src/clicodrome/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/souh/Bureau/S8/PDP/DjangoProject/clicodrome/src/clicodrome/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
