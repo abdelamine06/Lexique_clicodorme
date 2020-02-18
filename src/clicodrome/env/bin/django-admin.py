7cbcfe5f3e42d0af3fd21284c7ca0982cd30858e
seguerch <seguerch.el-guerch@etu.u-bordeaux.fr>
2020-02-18 19:08:17 +0100

    Trying to work for yassir

---
+++ src/clicodrome/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/souh/Bureau/S8/PDP/DjangoProject/clicodrome/src/clicodrome/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
